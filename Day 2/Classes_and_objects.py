405e7a0c4a64f51e28e0f95de7329bca17234bcc
anshika-1-2 <anshikapatel@vebuin.com>
2026-01-06 10:20:12 +0530

    adding classes and objects

---
+++ Day 2/Classes_and_objects.py
@@ -0,0 +1,14 @@
+#classes and objects
+class Dog:
+    species = "Canines"  
+
+    def __init__(self, name, age):
+        self.name = name  
+        self.age = age  
+
+dog1 = Dog("Rockie", 6)  
+dog2 = Dog("Bo", 2)  
+
+print(dog1.name, dog1.age, dog1.species)  
+print(dog2.name, dog2.age, dog2.species)  
+print(Dog.species)  
