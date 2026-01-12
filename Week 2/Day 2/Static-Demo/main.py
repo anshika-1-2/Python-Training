b90d583d2a920165a6b270c433c15002a23f915a
anshika-1-2 <anshikapatel@vebuin.com>
2026-01-12 11:27:40 +0530

    static file demo

---
+++ Week 2/Day 2/Static-Demo/main.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    """
+    Render the index.html template with a message.
+
+    Returns:
+        str: The rendered template as a string.
+    """
+    message = "Hello, World"
+    return render_template('index.html', message=message)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
+    
